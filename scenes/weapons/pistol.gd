4561305994096498b5133bc3c87fa9c5f11a8d22
Nathaniel Lackey <hutchkey@gmail.com>
2025-08-10 12:35:42 -0500

    Fixed bullet direction. Need to fix rotation

--- scenes/weapons/pistol.gd
+++ scenes/weapons/pistol.gd
@@ -14,15 +14,18 @@ const SMALL_BULLET = preload("res://scenes/weapons/small_bullet.tscn")
 var can_shoot := true
 var current_ammo := 15
 
-func shoot(direction: Vector2):
+func shoot(target_position: Vector2):
 	if can_shoot:
 		can_shoot = false
 		animation_player.play("shoot")
 		var bullet = SMALL_BULLET.instantiate()
 		var bullet_container = get_tree().get_first_node_in_group("bullet_container")
 		bullet_container.add_child(bullet)
-		bullet.direction = direction
 		bullet.global_position = bullet_spawn.global_position
+		
+		var dir = (target_position - bullet.global_position).normalized()
+		bullet.direction = dir
+		
 		fire_rate_timer.start(fire_rate)
 
 
